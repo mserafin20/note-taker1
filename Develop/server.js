945ea8dac7d86260c3c9c4e018c85a48bf46db4b
mserafin20 <mmserafin20@gmail.com>
2023-06-08 16:27:38 -0400

    added GET requests for index/notes.html files

--- Develop/server.js
+++ Develop/server.js
@@ -3,10 +3,22 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 4444;
 
-// Middleware
+// Middleware for parsing application/json and urlencoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 
+
+// GET request for index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+// GET request for notes.html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
 
 // Listening for requests
 app.listen(PORT, () => 
