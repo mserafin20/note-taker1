818060f787c9aa968899c519506ac33364894c86
mserafin20 <mmserafin20@gmail.com>
2023-06-12 12:28:55 -0400

    completed pathing for api and html files

--- Develop/routes/api-route.js
+++ Develop/routes/api-route.js
@@ -13,11 +13,29 @@ router.get('./notes', (req, res) => res.json(db));
 
 router.post('./notes', (req, res) => {
     const {title, text} = req.body;
+
         if(title && text) {
             const note = {title, text, id: uuid()}
-        };
+        
 
         db.push(note);
 
         const noteStringify = JSON.stringify(db);
-})
+
+        fs.writeFile('./db/db.json', noteStringify, (err) => 
+            err
+                ? console.log(err)
+                : console.log(
+                    `Note for ${note.title} has been saved!`
+                )
+        );
+        const response = {status: "successful", body: note};
+        console.log(response);
+        res.status(201).json(response);
+                } else {
+                    res.status(500).json("Encountered an error posting note");
+                }
+
+            });
+
+            module.exports = router;
