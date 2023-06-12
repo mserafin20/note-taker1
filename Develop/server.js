a84c3a4c23596094929ca20fbad51f1b9dbb728a
mserafin20 <mmserafin20@gmail.com>
2023-06-12 11:33:05 -0400

    added router and tested for functionallity

--- Develop/server.js
+++ Develop/server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 4444;
+const routes = require('./routes');
 
 // Middleware for parsing application/json and urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(routes);
 
 
 
